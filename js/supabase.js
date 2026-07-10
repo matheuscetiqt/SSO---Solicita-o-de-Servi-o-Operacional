f17d94ab78246e8f91807099336519ba469d86f5
matheuscetiqt <MDRConceicao@cetiqt.senai.br>
2026-07-10 12:08:07 -0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://yfgstgtcreilfgohfscs.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_kKX0CGxzEoQfOt6PRPSOeA_voCz5Z4A";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
