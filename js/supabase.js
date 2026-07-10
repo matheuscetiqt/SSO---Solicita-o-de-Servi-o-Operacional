586d38d956523c7b4a0170cf0514c0fbe6de4bb0
matheuscetiqt <MDRConceicao@cetiqt.senai.br>
2026-07-10 13:54:15 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,11 @@ const SUPABASE_URL = "https://yfgstgtcreilfgohfscs.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_kKX0CGxzEoQfOt6PRPSOeA_voCz5Z4A";
 
-const supabase = window.supabase.createClient(
+// A biblioteca expõe window.supabase
+const { createClient } = window.supabase;
+
+// Nosso cliente terá outro nome
+const supabaseClient = createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
