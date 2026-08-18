68fe546aeec4e283d003a03b9e4aadbf9b1de6c0
matheuscetiqt <MDRConceicao@cetiqt.senai.br>
2026-08-18 10:26:37 -0300

    Update solicitacao.js

--- js/solicitacao.js
+++ js/solicitacao.js
@@ -9,15 +9,6 @@ import {
     serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-import {
-    getStorage,
-    ref,
-    uploadBytes,
-    getDownloadURL
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-storage.js";
-
-const storage = getStorage();
-
 window.addEventListener("load", () => {
 
     carregarUsuario();
@@ -430,9 +421,6 @@ if (tipoServico === "Outro") {
         const protocolo =
             "SSO-" + Date.now();
 
-        const anexos = await enviarAnexos(protocolo);
-
-
         await addDoc(
             collection(db, "solicitacoes"),
             {
@@ -454,8 +442,6 @@ if (tipoServico === "Outro") {
                 status: "Pendente",
 
                 dataCriacao: serverTimestamp(),
-
-                anexos: anexos,
 
 
                 // ------------------------------
@@ -653,65 +639,3 @@ descricaoOutro:
     }
 
 });
-async function enviarAnexos(protocolo) {
-
-    const arquivos = [];
-
-    const camposArquivo = document.querySelectorAll(
-        'input[type="file"]'
-    );
-
-    camposArquivo.forEach((campo) => {
-
-        if (campo.files && campo.files.length > 0) {
-
-            for (let i = 0; i < campo.files.length; i++) {
-
-                arquivos.push(campo.files[i]);
-
-            }
-
-        }
-
-    });
-
-    if (arquivos.length === 0) {
-
-        return [];
-
-    }
-
-    const anexos = [];
-
-    for (const arquivo of arquivos) {
-
-        const caminho = `solicitacoes/${protocolo}/${arquivo.name}`;
-
-        const referenciaArquivo = ref(
-            storage,
-            caminho
-        );
-
-        await uploadBytes(
-            referenciaArquivo,
-            arquivo
-        );
-
-        const url = await getDownloadURL(
-            referenciaArquivo
-        );
-
-        anexos.push({
-
-            nome: arquivo.name,
-
-            url: url
-
-        });
-
-    }
-
-    return anexos;
-
-}
-   
